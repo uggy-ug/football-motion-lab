44edec8ef180103300063fd1b09300e1d49f36b3
Eugene Z. <72197489+uggy-ug@users.noreply.github.com>
2026-07-30 23:49:31 +0200

    Make Around the World the lead advanced motion

--- tricks.js
+++ tricks.js
@@ -1,4 +1,5 @@
+import { aroundTheWorld } from './around-the-world.js';
 import { trick as stepOver } from './trick.js';
 import { oneFootJuggling } from './juggling.js';
 
-export const tricks=[stepOver,oneFootJuggling];
+export const tricks=[aroundTheWorld,stepOver,oneFootJuggling];
